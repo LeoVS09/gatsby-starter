78d1b51ab5db3b94ec8d8f866bd805afd32f7a3a
leovs09 <leovs010@gmail.com>
2022-06-02 18:56:39 +0200

    fix issue with empty image on all posts page

---
+++ src/@lekoarts/gatsby-theme-minimal-blog-core/templates/blog-query.tsx
@@ -0,0 +1,30 @@
+import { graphql } from "gatsby"
+import BlogComponent from "@lekoarts/gatsby-theme-minimal-blog-core/src/components/blog"
+
+export default BlogComponent
+
+export const query = graphql`
+  query ($formatString: String!) {
+    allPost(sort: { fields: date, order: DESC }) {
+      nodes {
+        slug
+        title
+        date(formatString: $formatString)
+        excerpt
+        timeToRead
+        description
+        tags {
+          name
+          slug
+        }
+        banner {
+            childImageSharp {
+                resize(width: 1200, quality: 90) {
+                    src
+                }
+            }
+        }
+      }
+    }
+  }
+`
